661f44a8183271d11c3146a78bb692efe92f5c4a
Yunxiao Liu <yunxiao.liu@tum.de>
2025-05-09 23:28:04 +0200

    add load and save function to class sample

--- sample.py
+++ sample.py
@@ -1,13 +1,25 @@
 '''
-This modules defined each sample, their properties and their supported functions.
+This modules defined each sample, their properties
+and their supported functions.
 '''
+
+import pickle
+
+
 class Sample:
     def __init__(self, name, mass=None, make_time=None):
         self.name = name
         self.mass = mass
         self.make_time = make_time
-    
+
     def selfIntroduction(self):
         print(f'Hi I am {self.name}')
 
-    
+    def save(self):
+        with open(f"{self.name}.pkl", "wb") as f:
+            pickle.dump(self, f)
+
+    @staticmethod
+    def load(filepath):
+        with open(filepath, "rb") as f:
+            return pickle.load(f)
