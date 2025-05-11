1528a6da9677d34fac1bb890e3776d33ed92fcde
Yunxiao Liu <yunxiao.liu@tum.de>
2025-05-11 13:10:12 +0200

    record field_strength when init HCMeasurement

--- measurement/heat_capacity.py
+++ measurement/heat_capacity.py
@@ -1,13 +1,19 @@
 '''
 This module define the HeatCapacityMeasurment instance,
-which is a descendant of [Measurement]
+which is a descendant of [Measurement].
+
+Heat Capcity's experiment condition:
+[field_strength]
 '''
-import pandas as pd
 
 from .base import Measurement
 
 
 class HeatCapacityMeasurement(Measurement):
+    def __init__(self, filepath, field_strength: float = 0.0, metadata=None):
+        self.field_strength = field_strength
+        super().__init__(filepath, metadata)
+
     def _load_data(self):
         with open(file=self.filepath, encoding='ISO-8859-1') as f:
             content = f.readlines()
