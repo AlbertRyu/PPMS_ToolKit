887695bf7b62723320c3b423c724f1a394f379ab
Yunxiao Liu <yunxiao.liu@tum.de>
2025-05-20 16:38:05 +0200

    change __init__

--- src/ppms_toolkit/measurement/__init__.py
+++ src/ppms_toolkit/measurement/__init__.py
@@ -1,12 +1,17 @@
 # __init__.py
+
+# ruff: noqa
+# pylint: disable=unused-import
+
 from .base import Measurement
 from .heat_capacity import HeatCapacityMeasurement
+from .vsm import VSMMeasurement
 from .utils import merge_by_temp_diff, debye_model, debye_model_extended
 
 # When import with "from measurement import *",
 # * includes the following thing.
-__all__ = ["Measurement",
-           "HeatCapacityMeasurement",
-           "merge_by_temp_diff",
-           "debye_model",
-           "debye_model_extended"]
+# __all__ = ["Measurement",
+#            "HeatCapacityMeasurement",
+#            "merge_by_temp_diff",
+#            "debye_model",
+#            "debye_model_extended"]
