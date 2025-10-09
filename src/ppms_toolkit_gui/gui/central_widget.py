f1f4082dee869799a2757f1db610e781564023b7
Yunxiao Liu <yunxiao.liu@tum.de>
2025-10-09 14:42:48 +0200

    add checkbox to ListWidget

--- src/ppms_toolkit_gui/gui/central_widget.py
+++ src/ppms_toolkit_gui/gui/central_widget.py
@@ -1,6 +1,9 @@
 from ..adapters.mpl_canvas import MplCanvas
-from PySide6.QtWidgets import QWidget, QListWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel
+from PySide6.QtWidgets import \
+    (QWidget, QListWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel,
+     QAbstractItemView)
 
+from PySide6.QtCore import Qt
 
 class MyCenterWidget(QWidget):
     def __init__(self):
@@ -8,6 +11,8 @@ class MyCenterWidget(QWidget):
 
         self.canvas = MplCanvas()
         self.measurement_list = QListWidget(self)
+        self.measurement_list.setSelectionMode(
+            QAbstractItemView.SelectionMode.ExtendedSelection)
 
         # Add a default measurement for debug purpose.
         from ppms_toolkit.measurement import VSMMeasurement
@@ -21,6 +26,10 @@ class MyCenterWidget(QWidget):
         )
         item = QListWidgetItem(m.__repr__())
         item.setData(Qt.ItemDataRole.UserRole, m)
+        item.setFlags(item.flags() | Qt.ItemFlag.ItemIsUserCheckable 
+                      | Qt.ItemFlag.ItemIsSelectable 
+                      | Qt.ItemFlag.ItemIsEnabled)
+        item.setCheckState(Qt.CheckState.Unchecked)
         self.measurement_list.addItem(item)
         # End of Test Measurement
 
