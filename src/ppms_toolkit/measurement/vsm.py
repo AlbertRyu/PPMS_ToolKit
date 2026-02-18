aa8ad2da0ff97cf8af7ba2bbccbf80741549433f
yunxiao_liu <yunxiao.liu@tum.de>
2026-02-18 17:07:54 +0100

    better format for MT plot

--- src/ppms_toolkit/measurement/vsm.py
+++ src/ppms_toolkit/measurement/vsm.py
@@ -129,7 +129,7 @@ class VSMMeasurement(Measurement):
             if self.mode == 'MH':
                 label = f'{mid if mid else ''} {self.const_temp:.0f}K' 
             elif self.mode == "MT":
-                label = f'{mid}-{self.const_field}Oe - {self.condition} - {self.sample.orientation or "Unknown Ori"}' 
+                label = f'{mid if mid else ''} {self.const_field}Oe {self.condition} {self.sample.orientation or "Unknown Ori"}' 
         elif legend == 'Sample Name':
             label = f'{self.sample.name}'
         else:
